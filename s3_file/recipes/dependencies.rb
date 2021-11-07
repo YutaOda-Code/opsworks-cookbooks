1c1c8b04f8a9896c98ffd4f63e6e88d6cf8b8e32
YutaOda-Code <76253498+YutaOda-Code@users.noreply.github.com>
2021-11-07 17:07:28 +0900

    Update dependencies.rb

--- s3_file/recipes/dependencies.rb
+++ s3_file/recipes/dependencies.rb
@@ -1,5 +1,5 @@
-# chef_gem 'rest-client' do
-#   version node['s3_file']['rest-client']['version']
-#   action :install
-#   compile_time true if Chef::Resource::ChefGem.instance_methods(false).include?(:compile_time)
-# end
+ chef_gem 'rest-client' do
+   version node['s3_file']['rest-client']['version']
+   action :install
+   compile_time true if Chef::Resource::ChefGem.instance_methods(false).include?(:compile_time)
+ end
