ef768b1a3cd4a9f2a27bf6889f68e89c66e67ec0
YutaOda-Code <76253498+YutaOda-Code@users.noreply.github.com>
2021-11-14 14:45:56 +0900

    Update matchers.rb

--- s3_file/libraries/matchers.rb
+++ s3_file/libraries/matchers.rb
@@ -1,5 +1,5 @@
-if defined?(ChefSpec)
-  def create_s3_file(path)
-    ChefSpec::Matchers::ResourceMatcher.new(:s3_file, :create, path)
-  end
-end
+# if defined?(ChefSpec)
+#   def create_s3_file(path)
+#     ChefSpec::Matchers::ResourceMatcher.new(:s3_file, :create, path)
+#   end
+# end
